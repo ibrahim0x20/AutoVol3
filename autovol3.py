09e7c7640487e6e4e1582e168fd50110058c478f
Ibrahim Hakami <ebraheem.h@gmail.com>
2023-10-10 21:33:18 +0000

    Start working on malhandles function

--- autovol3.py
+++ autovol3.py
@@ -67,6 +67,7 @@ class AutoVol3(object):
     cmdline = {}
     netscan ={}
     getsids ={}
+    handles = {}
 
     baseline_proc = {}
     evidence_bag = []
@@ -101,6 +102,8 @@ class AutoVol3(object):
         self.csv_reader('cmdline.csv')
         self.csv_reader('netscan.csv')
         self.csv_reader('getsids.csv')
+        self.csv_reader('handles.csv')
+        self.malhandles()
 
         # Analyzing memory for suspicious processes
         self.procTree(os.path.join(self.app_path, args.p) + '/'+'pslist.csv')
@@ -567,6 +570,7 @@ class AutoVol3(object):
                     elif foreign_port in ["5985", "5986"] and not dst_ip.startswith(("0.0.0.0", "127.0.0.1")):
                         suspect_comm.append(','.join(row))
 
+            # suspect_comm is a temp variable collect suspecious procs for every for loop. At the end of every for loop it add the result to suspect_netscan.
             if suspect_comm:
                 if pid not in self.suspect_netscan:
                     self.suspect_netscan[pid] = []
@@ -595,6 +599,78 @@ class AutoVol3(object):
                 if process not in self.normal_sids[sid]:
                     row.append('Malicious Process: Uknown process running with system account')
                     self.suspecious_proc_sids[pid] =','.join(row)
+    def malhandles(self):
+        # 1. Identify Anomalous Handles:
+        #     Look for handles with unusual or unexpected types, such as file handles for system files or handles to uncommon objects.
+        #     Pay attention to handles associated with processes that are not part of the normal system configuration.
+
+        # 2. Examine Handle Counts:
+        #     Check for processes with an unusually high number of handles. This could indicate a process that is performing a lot of file or resource operations, which may be suspicious.
+
+        # 3. Investigate Suspicious File Handles:
+        #     Focus on file handles and look for processes that have handles to sensitive files, such as system binaries or critical configuration files.
+        #     Cross-reference file handles with file paths to determine if any files are accessed from suspicious locations.
+
+        # 4. Analyze Network-Related Handles:
+        #     Investigate handles related to network resources, such as sockets or network connections. Look for processes communicating with unusual IP addresses or ports.
+
+        # 5. Correlate with Process Information:
+        #     Correlate the handles with other process information like process names, PIDs, and parent-child relationships to identify any processes that appear suspicious or out of place.
+
+        # 6. Check for Code Injection:
+        #     Look for processes that have handles to memory sections with Execute (X) permissions. This could indicate code injection or process hollowing, common techniques used by malware.
+
+        # 7. Review Timestamps:
+        #     Analyze the timestamps associated with handles. Suspicious processes may have handles with timestamps that don't align with the system's normal behavior.
+
+        # 8. Cross-Reference with Known Malware Indicators:
+        #     Compare the handles and associated process information with known indicators of compromise (IOCs) and malware signatures to identify matches.
+
+        # 9. Analyze Parent-Child Relationships:
+        #     Look for unusual parent-child relationships between processes. Malware often spawns child processes to perform malicious activities, so identifying such relationships can be crucial.
+
+        # 10. Employ YARA Rules:
+        #     Use YARA rules to scan the memory and handles for specific patterns or signatures associated with known malware.
+
+        # 11. Leverage Threat Intelligence:
+        #     Consult threat intelligence feeds and databases to check if any of the handles or processes are associated with known malware campaigns or threat actors.
+
+        # 12. Behavioral Analysis:
+        #     Consider the overall behavior of processes with suspicious handles. Look for patterns that deviate from normal system behavior, such as excessive file manipulation or network communication.
+        
+        # Handles represent “pointers” to objects and it is not  unusual for a process to have a pointer to a file it is reading or writing to. However, 
+        # this is not the common way code is loaded into a process, so it is unusual to see a DLL file referenced in a process handle (recall that loaded
+        # DLLs are typically referenced in the Process Environment Block and displayed via the windows.dlllist plugin). 
+        # CLASSES Key Registry
+        # Storing scripts and data in randomly named registry keys has become a common technique for “fileless” malware, so it pays to keep an eye out strange key
+
+        # The interesting handle in the “msiexec.exe” PID 6192 process is a little more obvious. This process only had 16 file handles. If you think about the 
+        # purpose of “msiexec.exe” (installing .msi files), finding a reference to “flash_installer.msi” likely indicates what was installed on the system. 
+        # The name and folder location are of particular interest and ultimately led to recovery of the original infection vector.
+
+        # Named pipes can be identified via “File” handles in Volatility and thus when narrowing your focus, you can filter your output on that handle type 
+        # (this was not done in the example on the slide since output was filtered with grep for the text “pipe”).
+
+        # However, sometimes things stick out with some connected pipes appending IP addresses or process identifiers as part of the pipe name.
+
+        # Malware commonly sets mutants as a way of "marking" a compromised system to prevent reinfection. The malware will first check for the absence of 
+        # its mutant before performing an infection. During code analysis, reverse engineers can identify what mutant name is set and checked for by the malware. 
+
+        pattern = r'\.dll(?!\.mui)'
+
+        for pid in self.handles:
+            for row in self.handles[pid]:
+                #print(row)
+                if re.search(pattern, row[6]):
+                    print(row)
+                
+                if '.msi' in row[6]:
+                    print(row)
+
+                if 'NamedPipe' in row[6]:
+                    print(row)
+
+        print('Here will goes handles function')
 
 
     def csv_reader(self, csv_file):    
@@ -676,7 +752,7 @@ class AutoVol3(object):
                             if len(row) > pid_index:
                                 pid = row[pid_index]
                                 if pid not in self.dlllist:
-                                    self.dlllist[pid] = []
+                                    self.dlllist[pid] = []       # Every process PID has more than one DLL. Keep all dlls for every process in the same pid 
                             self.dlllist[pid].append(','.join(row))
 
                             if len(row) > path_index:
@@ -700,6 +776,21 @@ class AutoVol3(object):
                                     self.getsids[pid] = []
                             self.getsids[pid].append(','.join(row))
 
+                    elif csv_file == 'handles.csv':
+                        filter = ['File', 'Key', 'Mutant']  # 
+
+                        for row in csvreader:
+                            #print(','.join(row))
+                            if  len(row) == 0:
+                                continue
+                            if row[5] not in filter or row[7] == '':
+                                continue
+
+                            if len(row) > pid_index:
+                                pid = row[pid_index]
+                                if pid not in self.handles:
+                                    self.handles[pid] = []       # Every process PID has a lot of handles. Keep all handles for every process in the same pid 
+                            self.handles[pid].append(row[1:])
             
             except FileNotFoundError:
                 print(f"File '{csv_file}' not found.")
